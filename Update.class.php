58c3bdab71c22e56577c778099a71d7d7eec6790
fractalbit <fractalbit@gmail.com>
2019-05-18 16:44:53 +0300

    basic functionality in place

--- Update.class.php
+++ Update.class.php
@@ -4,16 +4,55 @@ require_once 'Parsedown.php';
 
 class App {
 
-    private $version_tag = '2.2.0';
+    private $current_version = '3.0.0'; 
+    // This should be changed every time we push to github
+    // while also creating the actual correspoding git tag
+    // And don't foget to push tags to github
+
+    private $app_dir = '';
+
+    private $folders = array();
+
+    private $simulate = false;
+    private $simulate_ver = 'v2.0.0';
+    private $simulate_dir = 'simulate/';
 
     private $releases = array();
     private $tags = array();
     private $commits = array();
 
     private $repo_raw_data = array();
+
+    private $repo; // This will hold the github instance
     
+    public function __construct($github_app_repo){
+        $this->app_dir = trailingslashit(dirname(__FILE__));
+        
+        if(is_dir('.git')){ // This means we are in a dev enviroment. We should be carefull not to overwrite our working directory!
+            $this->simulate = true;
+
+            $this->app_dir  = $this->app_dir . $this->simulate_dir;
+            $this->current_version = $this->simulate_ver;
+        }
+
+        $this->folders = array(
+            'updates'   => $this->app_dir . 'updates/',    
+            'releases'  => $this->app_dir . 'updates/releases/',    
+            'downloads' => $this->app_dir . 'updates/downloads/',    
+        );
+
+        // I should create the folders if they do not exist
+
+        dump($this->app_dir);
+        dump($this->current_version);
+        dump($this->folders);
+
+        $this->repo = $github_app_repo;
+    }
+
+
     public function get_raw_data(){       
-        $repo = new Github();
+        $repo = $this->repo;
         // I don't think we have to download the commits
         // We will work with tags for version checking and releases if there is one for that tag (for better changelong)
         // $this->repo_raw_data['commits'] = json_decode($repo->get('commits'), true); // Set second parameter to true to return an assosiative array instead of an object
@@ -62,7 +101,60 @@ class App {
         return $date->format("d/m/Y H:i");
     }
 
-    
+    public function update(){
+        // Lots more to do. Just a test
+
+        // Get the correct version. Maybe just get the latest is enough for our purposes
+        $latest = json_decode($this->repo->get('releases/latest'), true);
+        // dump($latest);
+        $zipball = $latest['zipball_url'];
+        dump($zipball);
+        // We could just hardcode this url: https://github.com/fractalbit/misthodosia-online/archive/master.zip
+        // To get the latest pushed commit.
+        // But maybe it's better to download specific versions
+        // So there is always a direct assosciation between the current app version and what we get from github
+
+        // Download the target version as zip
+        $download = $this->repo->get_release($zipball);
+        $release_filename = $this->folders['downloads'] . 'misthodosia-online-' . $latest['tag_name'] . '.zip';
+        dump($release_filename);        
+        file_put_contents($release_filename, $download);
+
+        // Unzip the file we just downloaded
+        $zip = new ZipArchive;
+        $res = $zip->open($release_filename);
+        if ($res === TRUE) {
+            $zip->extractTo($this->folders['releases'] . $latest['tag_name']);
+            $zip->close();
+            echo 'Το αρχείο αποσυμπιέστηκε με επιτυχία';            
+        } else {
+            echo 'Συνέβη ένα σφάλμα κατά την αποσυμπίεση του αρχείου';   
+        }
+
+        // Get the directory which contents we want to copy
+        $directories = glob($this->folders['releases'] . $latest['tag_name'] . '/*' , GLOB_ONLYDIR);
+        $source_dir = $directories[0];
+        
+        // Aaaaaand copy the files to the app_dir folder ... and pray!
+        
+        dump($source_dir);
+        dump($this->app_dir);
+        xcopy($source_dir, $this->app_dir);
+
+        // Delete the .gitignore files from the simulate directory
+        // if($this->simulate){
+        //     $for_del = glob($this->simulate_dir . '**/*/.gitignore');
+        //     dump($for_del);
+        //     // foreach($for_del as $file)
+        // }
+               
+        // Maybe also change directory permissions after copying
+    }
+
+    private function cleanup(){
+        // Delete contents of the updates folder
+        // Maybe keep the last version though?
+    }
 }
 
 class Github {
@@ -92,10 +184,6 @@ class Github {
         }
     }
 
-    public function download($tag){
-        // Download the specified tag version from github
-    }
-
     public function unzip(){
         // Just try the unzip for now with hardcoded file and path
         $zip = new ZipArchive;
@@ -115,7 +203,26 @@ class Github {
         }
     }
 
+    public function get_release($fetch_url){
+        // Download the specified tag version from github
 
+        $ch = curl_init();
+
+        // Set curl options
+        curl_setopt($ch, CURLOPT_URL, $fetch_url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);               
+        curl_setopt($ch, CURLOPT_USERAGENT, 'curl/' . curl_version()['version']); // Απαραίτητο να στείλουμε έναν useragent στο github api για να δουλέψει
+
+        $result = curl_exec($ch);
+        curl_close($ch);        
+        
+        if($result === FALSE) {
+            echo "Error: " . curl_error($ch);
+        } else {            
+            return $result;
+        }
+    }
 }
 
 /**
@@ -175,11 +282,15 @@ function xcopy($source, $dest, $permissions = 0755)
 
 <?php
 
-// $mo = new App;
+$repo = new Github();
+$mo = new App($repo);
+$mo->update();
 // $mo->get_raw_data();
 
-$repo = new Github();
-$repo->unzip();
+// $repo->get_release('https://api.github.com/repos/fractalbit/misthodosia-online/zipball/v2.1.1');
+// $repo->unzip();
+
+
 
 // Just a quick test about versioning and version comparison
 // $app_versions = array('v2.2.0', 'v2.2.1', 'v2.3', 'v2.3.1', 'v2.3.1', 'v2.3.0.9');
