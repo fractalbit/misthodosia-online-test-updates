9a0d0df97ec76d99e3f8d1ba29b6575e31dc98b3
fractalbit <fractalbit@gmail.com>
2019-05-20 20:37:27 +0300

    added comments for session handling

--- update.php
+++ update.php
@@ -14,7 +14,11 @@ if(admin_configured()){
         $mo->get_releases();
         $mo->print_newer();
         fSession::set('app', $mo); // Store the App object so that ajax requests can access it
-           
+        // This does not work well when sqitching branches
+        // I should see how i can delete session data from xampp
+        // Or consider other ways to save data to the server
+        // Maybe store in session only the data i want and not the full object
+        // Or pass the required data from one ajax request to another   
     }else{
         echo '<div class="error">'.$admin->message.'</div>';
         echo $admin->show_login_form();
