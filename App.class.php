85efcdb17bd31261ca1d520c459b09d0e465d211
fractalbit <fractalbit@gmail.com>
2019-06-02 01:11:37 +0300

    simple test

--- App.class.php
+++ App.class.php
@@ -31,7 +31,7 @@ class App
             $this->simulate = true;
             $this->app_dir  = $this->app_dir . $this->simulate_dir;
             $this->current_version = $this->simulate_ver;
-            $this->repo_url = 'https://api.github.com/repos/fractalbit/misthodosia-online-test-updates/';
+            $this->repo_url = 'https://api.github.com/repos/fractalbit/misthodosia-online-test-updates/'; // Move this outside of if to check for updates in the alternate repo
         }
 
         $this->folders = array(
